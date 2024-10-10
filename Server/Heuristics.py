77fc078e0e79e51832b6848702ddc1143aa4f46f
Tomas Daniel <tomdaniel@email.cz>
2024-10-10 21:41:05 +0200

    Create known exchanges list from aggregated network resources.

--- Server/Heuristics.py
+++ Server/Heuristics.py
@@ -19,3 +19,12 @@ class HeuristicsClass():
         # Add result to UI
         self.ui.addResultAddress(retVal)
 # End of HeuristicsClass class
+
+# Workflow:
+# request lates labelled exchanges addresses from :
+        #https://blockchain.info/de/tags
+        #http://bitcoinwhoswho.com
+        #https://www.walletexplorer.com
+# find all addresses transfering funds to exchange addresses, but exclude ones same as exchange addresses
+# deposit address must forward constantly to the same exchange address
+# addresses sending to deposit addresses must be EOA, nor smart contracts, exchange or miner
