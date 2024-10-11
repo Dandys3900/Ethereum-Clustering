533705a491b3eaa40b8f1449f736a15235b3a86f
Tomas Daniel <tomdaniel@email.cz>
2024-10-11 15:53:37 +0200

    Initial version of Deposit Address Reuse heuristics.

--- Server/Heuristics.py
+++ Server/Heuristics.py
@@ -1,7 +1,8 @@
 # Performs clustering heuristics around target address
 # Imports
+import csv
 from GUI import App
-from .ServerData_Handler import ServerHandler
+from .ServerData_Handler import ServerHandler, startThreading
 
 class HeuristicsClass():
     def __init__(self, ui:App=None):
@@ -14,17 +15,36 @@ class HeuristicsClass():
     def clusterAddrs(self):
         # Load target address inserted by user
         targetAddr = self.ui.search_bar.get()
-        ### MOCK SETUP ###
-        retVal = self.api.getTransaction("0x676621c4a9d7b9f9d898254f190376fa17209651c387b4becf59ca430e1651ff")
+        # Create lists for storing transactions of target address and known exchanegs
+        addrTxs = []
+        dexTxs  = []
+        # Get all transactions for address and known exchanges
+        with open("exchanges.csv", "r", newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            # Construct functions list
+            funcsList = [
+                (self.api.getAccountTxs(targetAddr, addrTxs)),
+                (self.api.getAccountTxs(dexAddr, dexTxs) for dexAddr in reader["address"])
+            ]
+            # Execute address collecting
+            startThreading(funcsList)
+            # From both lists exclude known exchange addresses
+            addrTxs = filter(lambda x: x not in reader["address"], addrTxs)
+            dexTxs  = filter(lambda x: x not in reader["address"], dexTxs)
+        # Find all similar addresses
+        sameAddrs = list(set(addrTxs) & set(dexTxs))
+        # From found similar addresses get all addresses sending to them -> results
+        results = []
+        funcsList = [
+            (self.api.getAccountTxs(depositAddr, results) for depositAddr in sameAddrs)
+        ]
+        # Execute address collecting
+        startThreading(funcsList)
         # Add result to UI
-        self.ui.addResultAddress(retVal)
+        self.ui.addResultAddress(results)
 # End of HeuristicsClass class
 
 # Workflow:
-# request lates labelled exchanges addresses from :
-        #https://blockchain.info/de/tags
-        #http://bitcoinwhoswho.com
-        #https://www.walletexplorer.com
 # find all addresses transfering funds to exchange addresses, but exclude ones same as exchange addresses
 # deposit address must forward constantly to the same exchange address
 # addresses sending to deposit addresses must be EOA, nor smart contracts, exchange or miner
--- Server/ServerData_Handler.py
+++ Server/ServerData_Handler.py
@@ -1,7 +1,20 @@
 # Requests data for UI from server and handles response
 # Imports
-from Helpers import Out
+from Helpers import Out, startThreading
 from .API import *
+
+# From given transactions, extract destination addresses
+def gettxAddrs(apiResponse, addr, dest):
+    for tx in apiResponse:
+        vin = tx.get("vin")
+        if vin and vin[0].get("addresses")[0] == addr:
+            # Store destination address
+            dest.append(tx.get("vout")[0].get("addresses")[0])
+        # Or where it sends tokens via smart contract (if any)
+        tokenTrf = tx.get("tokenTransfers")
+        if tokenTrf and tokenTrf[0].get("from") == addr:
+            # Store destination address
+            dest.append(tokenTrf[0].get("to"))
 
 class ServerHandler():
     def __init__(self):
@@ -11,14 +24,36 @@ class ServerHandler():
             self.trezor = TrezorAPI()
         except Exception as e:
             Out.error(e)
-            # Exit when API error
+            # Exit on API error
             exit(-1)
 
-    # Gets transaction from server and adds it to UI
-    def getTransaction(self, txid=""):
-        retVal = self.trezor.get(f"tx/{txid}")
-        ### MOCK SETUP ###
+    # Returns current highest available block
+    def getBlockNums(self):
+        retVal = self.trezor.get("status")
         if retVal:
-            # Get and return vin address
-            return retVal.get("vin")[0].get("addresses")[0]
+            return retVal.get("blockbook", {}).get("bestHeight")
+        # Invalid value
+        return None
+
+    # Collects all transactions made by given addresses
+    def getAccountTxs(self, addr, results:list):
+        getTx = lambda results, addr, interval: results.extend(
+            gettxAddrs(
+                self.trezor.get(f"v2/address/{addr}", params={
+                    "from"    : interval[0],
+                    "to"      : interval[1],
+                    "details" : "txslight"
+                }),
+                addr,
+                results
+            )
+        )
+        step = 1000
+        # Generate functions list
+        funcList = [
+            getTx(results, addr, (start, start + (step - 1)))
+                for start in range(0, self.getBlockNums(), step)
+        ]
+        # Divide each segment into thread
+        startThreading(funcList)
 # End of ServerHandler class
