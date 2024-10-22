847d9e3cbbf983b4195918ecd551faf1a767a3d6
Tomas Daniel <tomdaniel@email.cz>
2024-10-22 23:00:35 +0200

    Integrate NebulaJS to the project.

--- Server/Heuristics.py
+++ Server/Heuristics.py
@@ -2,36 +2,111 @@
 # Imports
 import json
 from aiohttp import ClientSession
+from nebula3.gclient.net import ConnectionPool
+from nebula3.Config import Config
 from GUI import App
-from .ServerData_Handler import ServerHandler, partial, asyncio
+from Helpers import Out
+from .ServerData_Handler import ServerHandler, partial
 
 class HeuristicsClass():
     def __init__(self, ui:App=None):
         # Store UI instance
         self.ui = ui
-        # Init ServerData_Handler for communicating with blockchain client
-        self.api = ServerHandler()
         # Load list of all known exchange addresses
         with open("exchanges.json", "r", encoding="utf-8") as file:
             self.exchAddrs = json.load(file)
-        # Load all addresses linked with known exchanges
-        with open("exchanges_conns.json", "r", encoding="utf-8") as file:
-            self.exchConns = json.load(file)
+        # Init ServerData_Handler for communicating with blockchain client
+        self.api = ServerHandler([addr[0] for addr in self.exchAddrs.items()])
+
+    # Init connection to Nebula instance and return pool to use
+    def getNebulaPool(self):
+        try:
+            # Define config
+            config = Config()
+            # Init connection pool
+            connection_pool = ConnectionPool()
+            # Check if connection to pool is valid
+            if not connection_pool.init([('127.0.0.1', 9669)], config):
+                raise Exception("connection_pool.init() returned False")
+            return connection_pool
+        except Exception as e:
+            Out.error(f"Error while creating Nebula connection: {e}")
+            exit(-1)
+
+    def getAddrsOfType(self, addrType="", nebula_session=None):
+        # Make query to get all addresses of given type
+        result = nebula_session.execute(
+            f"LOOKUP ON address WHERE address.type == '{addrType}' YIELD address.address"
+        )
+        # Check if any addresses found
+        if result.is_empty():
+            Out.error(f"No addresses of type {addrType} found")
+            return []
+        return result.rows()
+
+    async def addExchanges(self, nebula_session=None):
+        # Add all exchanges to graph
+        await self.api.runParalel([
+            partial(
+                self.api.addNodeToGraph,
+                dexAddr,
+                dexName,
+                nodeType="exchange",
+                nebula=nebula_session
+            ) for dexAddr, dexName in self.exchAddrs.items()
+        ])
+
+    async def addDepositAddrs(self, nebula_session=None):
+        # Get all found deposit addresses
+        exchAddrs = self.getAddrsOfType("exchange", nebula_session)
+        # Create session for async requests
+        async with ClientSession() as trezor_session:
+            # Add all addresses interacting with known exchanges -> deposit addresses
+            await self.api.runParalel([
+                partial(
+                    self.api.getLinkedAddrs,
+                    trezor_session,
+                    dexAddr,
+                    nodeType="deposit",
+                    nebula=nebula_session
+                ) for dexAddr in exchAddrs
+            ])
+
+    async def addClusteredAddrs(self, nebula_session=None):
+        # Get all found deposit addresses
+        exchDepos = self.getAddrsOfType("deposit", nebula_session)
+        # Create session for async requests
+        async with ClientSession() as trezor_session:
+            # Add all addresses interacting with known exchanges -> deposit addresses
+            await self.api.runParalel([
+                partial(
+                    self.api.getLinkedAddrs,
+                    trezor_session,
+                    depoAddr,
+                    nodeType="leaf",
+                    nebula=nebula_session
+                ) for depoAddr in exchDepos
+            ])
 
     # Performs update of addresses connected to known exchanges
     async def updateExchangeConns(self):
-        self.exchConns = []
-        # Create session for async requests
-        async with ClientSession() as session:
-            # Execute address collecting
-            await self.api.runParalel([
-                partial(self.api.getLinkedAddrs, session, dexAddr, self.exchConns) for dexAddr in self.exchAddrs
-            ])
-            # Exclude known exchange addresses
-            self.exchConns = list(filter(lambda x: x not in self.exchAddrs, self.exchConns))
-        # Update addresses in JSON file
-        with open("exchanges_conns.json", "w", encoding="utf-8") as file:
-            json.dump(self.exchConns, file)
+        pool = self.getNebulaPool()
+        with pool.session_context('root', 'nebula') as nebula_session:
+            # Clear existing data
+            nebula_session.execute("CLEAR SPACE IF EXISTS EthereumClustering")
+
+            # Use defined space
+            nebula_session.execute('USE EthereumClustering')
+            # Create necessary tags and edges
+            nebula_session.execute("CREATE TAG IF NOT EXISTS address (name string, type string)")
+            nebula_session.execute("CREATE EDGE IF NOT EXISTS linked_to ()")
+
+            # Execute pipeline to construct graph
+            await self.addExchanges(nebula_session)
+            #await self.addDepositAddrs(nebula_session)
+            #awaitself.addClusteredAddrs(nebula_session)
+        # close the pool
+        pool.close()
 
     # Performs clustering around target address
     async def clusterAddrs(self):
@@ -40,29 +115,18 @@ class HeuristicsClass():
         # Avoid usage of empty address value
         if not targetAddr:
             return
-        # Create lists for storing transactions of target address and known exchanegs
-        addrTxs = []
-        # Create session for async requests
-        async with ClientSession() as session:
-            # Execute address collecting
-            await self.api.runParalel([
-                partial(self.api.getLinkedAddrs, session, targetAddr, addrTxs)
-            ])
-            # Exclude known exchange addresses
-            addrTxs = list(filter(lambda x: x not in self.exchAddrs, addrTxs))
-            # Find all similar addresses => deposit addresses
-            depositAddrs = list(set(addrTxs) & set(self.exchConns))
-            # None found -> return
-            if not depositAddrs:
-                return
-            # From found similar addresses get all addresses sending to them -> results
-            results = []
-            # Execute address collecting
-            await self.api.runParalel([
-                partial(self.api.getLinkedAddrs, session, depositAddr, results) for depositAddr in depositAddrs
-            ])
+        # Get all clustered addresses and check if target address is in any
+        pool = self.getNebulaPool()
+        with pool.session_context('root', 'nebula') as nebula_session:
+            # Use defined space
+            nebula_session.execute('USE EthereumClustering')
+
+            if targetAddr in self.getAddrsOfType("leaf", nebula_session):
+                print("FOUND")
+        # close the pool
+        pool.close()
         # Add result to UI
-        self.ui.addResultAddress(results)
+        #self.ui.addResultAddress(results)
 # End of HeuristicsClass class
 
 # Workflow:
--- Server/ServerData_Handler.py
+++ Server/ServerData_Handler.py
@@ -6,9 +6,12 @@ from Helpers import Out
 from .API import *
 
 class ServerHandler():
-    def __init__(self):
-        try: # Create API instances
+    def __init__(self, exchAddrs):
+        try:
+            # Create API instance
             self.trezor = TrezorAPI()
+            # Store list of known exchanges
+            self.exchAddrs = exchAddrs
         except Exception as e:
             Out.error(e)
             # Exit on API error
@@ -19,9 +22,20 @@ class ServerHandler():
         tasks = [func() for func in funcsList]
         return await asyncio.gather(*tasks)
 
+    # Handles adding new node to graph
+    async def addNodeToGraph(self, addr="", addrName="", parentAddr="", nodeType="", nebula=None):
+        nebula.execute(
+            f"INSERT VERTEX IF NOT EXISTS address(name, type) VALUES '{addr}': ('{addrName}', '{nodeType}')"
+        )
+        # Parent address is given so create a path to it
+        if parentAddr:
+            nebula.execute(
+                f"INSERT EDGE IF NOT EXISTS linked_to() VALUES '{parentAddr}'->'{addr}': ()"
+            )
+
     # From given transactions, extract addresses
     # NOTE: Store opposite address to found one in transaction
-    async def getTxAddrs(self, session, addr, results:list, page=1):
+    async def getTxAddrs(self, session=None, addr="", addrName="", parentAddr="", nodeType="", nebula=None, page=1):
         apiResponse = await (self.trezor.get(session, f"v2/address/{addr}", params={
             "page"    : page,
             "details" : "txslight"
@@ -36,16 +50,23 @@ class ServerHandler():
             try:
                 txFROMAddr = tx.get("vin")[0].get("addresses")[0]
                 txTOAddr   = tx.get("vout")[0].get("addresses")[0]
-                # Transaction contain target address
-                if txTOAddr and addr in [txFROMAddr, txTOAddr]:
-                    results.append(
-                        txTOAddr if addr == txFROMAddr else txFROMAddr
+                # Transaction contain target address and it's NOT known exchange
+                if addr in [txFROMAddr, txTOAddr] and addr not in self.exchAddrs:
+                    # Determine which address record to add
+                    addrKey = (txTOAddr if addr == txFROMAddr else txFROMAddr)
+                    # Add address to graph
+                    self.addNodeToGraph(
+                        addrKey,
+                        addrName,
+                        parentAddr,
+                        nodeType,
+                        nebula
                     )
             except Exception:
                 continue
 
     # Collects all addresses targetAddr has any transactions with
-    async def getLinkedAddrs(self, session, targetAddr, results:list):
+    async def getLinkedAddrs(self, session=None, targetAddr="", targetName="", parentAddr="", nodeType="", nebula=None):
         retVal = await self.trezor.get(session, f"v2/address/{targetAddr}", params={
             "details"  : "txslight"
         })
@@ -55,11 +76,27 @@ class ServerHandler():
         # Get total number of pages of transaction for target address
         totalPages = retVal.get("totalPages")
         # Process returned transactions (implicitily from first page)
-        await self.getTxAddrs(session, targetAddr, results)
+        await self.getTxAddrs(
+            session,
+            targetAddr,
+            targetName,
+            parentAddr,
+            nodeType,
+            nebula
+        )
         # Decide whether multi-thread
         if totalPages > 1:
             # Execute address collecting
             await self.runParalel([
-                partial(self.getTxAddrs, session, targetAddr, results, page) for page in range(2, (totalPages + 1))
+                partial(
+                    self.getTxAddrs,
+                    session,
+                    targetAddr,
+                    targetName,
+                    parentAddr,
+                    nebula,
+                    nodeType,
+                    page
+                ) for page in range(2, (totalPages + 1))
             ])
 # End of ServerHandler class
