b0e9c815dffa9a9ab31e0cf65030f257a7ced300
Dandys <tomasdaniel7800@gmail.com>
2025-01-19 12:15:24 +0100

    Remove desktop GUI.

--- GUI/__init__.py
+++
@@ -1,7 +0,0 @@
-# Collect all imports from current folder to allow easier work throught project
-import customtkinter as ct
-import tkinter       as tk
-
-from .Renderer        import App
-from .Element_Creator import ElementCreator
-from .Event_Handler   import EventHandler
