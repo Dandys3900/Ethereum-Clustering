1096ed95c961d7a4ac118660f88675301c9ad5ca
Dandys3900 <tomdaniel@email.cz>
2025-09-12 10:56:46 +0200

    Cosmetic changes more or less.

--- Client/static/JS/contentCreator.js
+++ Client/static/JS/contentCreator.js
@@ -13,3 +13,85 @@ function createModal ({style="", id="", header="", body="", bodyId="", footer=""
     // Append to body's end
     document.body.insertAdjacentHTML('beforeend', modalHTML);
 }
+
+function createResultsTable () {
+    return (window.resTable = new gridjs.Grid({
+        columns: [
+            "Entity"
+        ],
+        data: Object.keys(nodesParams).map(key => [
+            gridjs.html(
+                `<span style="display: inline-block; width: 10px; height: 10px; background-color: ${nodesParams[key].style.color}; border-radius: 2px;"></span>
+                ${key}`
+            )
+        ]),
+        pagination: {
+            limit: 20
+        },
+        search: true
+    }));
+}
+
+function createExchListTable (loggedIn, exchListData) {
+    return (window.exchTable = new gridjs.Grid({
+        columns: [
+            {
+                name : "Exchange address",
+                attributes: {
+                    style: "width: 50%;"
+                }
+            },
+            "Exchange name",
+            ...(loggedIn ? [{
+                    name     : "Actions",
+                    formatter: (_, row) => {
+                        return gridjs.html(
+                            `<div style="display: flex; gap: 10px; cursor: pointer;">
+                                <b><a href='https://etherscan.io/address/${row.cells[0].data.toLowerCase()}' target='_blank'>🔍</a></b>
+                                <b><a onclick='showEditModal("${row.cells[0].data}", "${row.cells[1].data}")'>🖉</a></b>
+                                <b><a onclick='deleteExchAdr("${row.cells[0].data}")'>🗑️</a></b>
+                            </div>`
+                        );
+                    }
+                }] : [])
+        ],
+        data: Object.keys(exchListData).map(key => {
+            // Key: Exch addr ; Value: Exch name
+            return [key, exchListData[key]];
+        }),
+        pagination: {
+            limit: 20
+        },
+        resizable: true,
+        search   : true
+    }));
+}
+
+function createTxTable () {
+    return (window.txTable = new gridjs.Grid({
+        columns: [
+            {
+                name : "Transaction",
+                attributes: {
+                    style: "width: 50%;"
+                }
+            },
+            "Time",
+            "Amount",
+            {
+                name     : "Actions",
+                formatter: (_, row) => {
+                    return gridjs.html(
+                        `<b><a href='https://etherscan.io/tx/${row.cells[0].data}' target='_blank' label='Show on EtherScan'>🕵️</a></b>`
+                    );
+                }
+            }
+        ],
+        data: [],
+        pagination: {
+            limit: 20
+        },
+        resizable: true,
+        search   : true
+    }));
+}
